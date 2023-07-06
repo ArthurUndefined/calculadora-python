c36047102babf3c8cb7b683d987611ce5cb8813a
arthur_rabell <lontra15951@gmail.com>
2023-07-06 14:38:56 +0000

    update 2

--- script.py
+++ script.py
@@ -1,38 +1,32 @@
 def main():
     expressão = input("Digite a expressão matematica:").split(" ")
     
+    def inserirResultado(index, resultado):
+            for x in range(3):
+                expressão.pop(index-1)
+            expressão.insert(index-1, resultado)
+            operacoes()
+
     def operacoes():
         if "*" in expressão:
             index = expressão.index("*")
             resultado = int(expressão[index-1]) * int(expressão[index+1])
-            for x in range(3):
-                expressão.pop(index-1)
-            expressão.insert(index-1, resultado)
-            operacoes()
+            inserirResultado(index, resultado)
 
         if "/" in expressão:
             index = expressão.index("/")
             resultado = int(expressão[index-1]) / int(expressão[index+1])
-            for x in range(3):
-                expressão.pop(index-1)
-            expressão.insert(index-1, resultado)
-            operacoes()
+            inserirResultado(index, resultado)
 
         if "+" in expressão:
             index = expressão.index("+")
             resultado = int(expressão[index-1]) + int(expressão[index+1])
-            for x in range(3):
-                expressão.pop(index-1)
-            expressão.insert(index-1, resultado)
-            operacoes()
+            inserirResultado(index, resultado)
 
         if "-" in expressão:
             index = expressão.index("-")
             resultado = int(expressão[index-1]) - int(expressão[index+1])
-            for x in range(3):
-                expressão.pop(index-1)
-            expressão.insert(index-1, resultado)
-            operacoes()
+            inserirResultado(index, resultado)
 
     operacoes()
     
